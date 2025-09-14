109a30687b2329b9c44aae8a81100efbf0e3eb76
Zuzu <165035502+ZuanCrisp@users.noreply.github.com>
2025-09-14 10:57:53 +0700

    BlockTheSpot + Spicetify.bat

--- BlockTheSpot + Spicetify.bat
+++ BlockTheSpot + Spicetify.bat
@@ -4,7 +4,7 @@ set /p UserInput= "Spicetify will be Installed. If you don't agree, use the Bloc
 if /i "%UserInput%"=="y" (
     powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex}"
     powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex}"
-    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1' | Invoke-Expression}"
+    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/ZuanCrisp/BlockTheSpot/master/install.ps1' | Invoke-Expression}"
     pause
 ) else (
     echo "Patch not installed."
